b4e6b0a136d5d1983f1dc7e912dd2052ebb74e50
Dudullll <vitoreduar05@gmail.com>
2026-06-08 19:06:43 -0300

    config mini jogos

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="h-full antialiased">
-      <body className="min-h-full flex flex-col bg-black text-white">
+      <body className="min-h-full flex flex-col bg-black text-white overflow-x-clip">
         <AuthProvider>
           <CasinoBackground />
           <Navbar />
